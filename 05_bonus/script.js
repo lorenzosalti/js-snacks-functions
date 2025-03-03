b454ecfe5aebf61d99a26a3e9f846afbe7b332cb
lorenzosalti <lorenzosalti@gmail.com>
2025-03-03 17:31:18 +0100

    feat: bonus

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,29 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const userName = 'Mario';
 
+const systemDate = new Date();
+const time = systemDate.getHours();
 
 // Dichiara la funzione qui.
+
+function helloTime(name) {
+  let helloName = '';
+  if (time >= 13 && time < 17) {
+    helloName = 'Buon pomeriggio ' + name;
+  } else if (time < 13) {
+    helloName = 'Buongiorno ' + name;
+  } else {
+    helloName = 'Buonasera ' + name;
+  }
+  return helloName;
+}
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(helloTime(userName));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
