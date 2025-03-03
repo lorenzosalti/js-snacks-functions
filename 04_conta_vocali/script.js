1b0ba6e07f71a8c5bd3d0ce39f776cb5dc086a74
lorenzosalti <lorenzosalti@gmail.com>
2025-03-03 16:56:36 +0100

    feat: conta vocali

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,25 @@
 
 const word = 'javascript';
 
+const vowels = ['a', 'e', 'i', 'o', 'u'];
 
 // Dichiara la funzione qui.
 
+function vowelsNum(word) {
+  let result = 0;
+  word = word.toLowerCase();
+  for (let i = 0; i < word.length; i++) {
+    let isVowel = false;
+    for (let index = 0; index < vowels.length; index++) {
+      if (word[i] === vowels[index]) isVowel = true;
+    }
+    if (isVowel) result++;
+  }
+  return result;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(vowelsNum(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
